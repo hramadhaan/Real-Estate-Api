f3977b05c9bf0cb7f2b9aebd989608d622ac0fdd
hramadhaan <hramadhaan20@gmail.com>
2020-08-01 17:38:22 +0700

    Gatau komitan yg mana

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 // PACKAGE
 const express = require("express");
 const multer = require("multer");
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 
 // FILE
 // const User = require("../models/user");
@@ -13,18 +13,18 @@ const userController = require("../controllers/user");
 const authentication = require("../middleware/authentication");
 
 const fileStorage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "images/profile/");
-    },
-    filename: function(req, file, cb) {
-        cb(null, uuidv4() + file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, "images/profile/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + " - " + file.originalname);
+  },
 });
 
 router.post(
-    "/register",
-    multer({ storage: fileStorage }).single("photo"),
-    userController.register
+  "/register",
+  multer({ storage: fileStorage }).single("photo"),
+  userController.register
 );
 
 router.post("/login", userController.login);
